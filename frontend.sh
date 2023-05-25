8cd7093d8d73fe2a3e8597eab95bb2941d547f3b
Raghu K <raghuk.vit@gmail.com>
2023-05-25 08:39:14 +0530

    RoboShop nginx Script

--- frontend.sh
+++ frontend.sh
@@ -5,7 +5,7 @@ echo -e "\e[33mRemoving Old App content\e[0m"
 rm -rf /usr/share/nginx/html/* >/tmp/roboshop.log
 
 echo -e "\e[33mDownloading Frontend Content\e[0m"
-curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend.zip >/tmp/roboshop.log
+curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend.zip >/tmp/roboshop.log 2>/tmp/error.log
 
 echo -e "\e[33mExtract Frontend Content\e[0m"
 cd /usr/share/nginx/html
