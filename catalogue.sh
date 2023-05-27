b94e4be0da3ea91961a96a9021095554ac03dbf1
Raghu K <raghuk.vit@gmail.com>
2023-05-27 08:44:52 +0530

    Payment

--- catalogue.sh
+++ catalogue.sh
@@ -2,12 +2,4 @@ source common.sh
 component=catalogue
 
 nodejs
-
-echo -e "${color} Copy MongoDB Repo file ${nocolor}"
-cp /home/centos/roboshop-shell/mongodb.repo /etc/yum.repos.d/mongodb.repo  &>>$log_file
-
-echo -e "${color} Install MongoDB Client ${nocolor}"
-yum install mongodb-org-shell -y  &>>$log_file
-
-echo -e "${color} Load Schema ${nocolor}"
-mongo --host mongodb-dev.devopsb73.store <${app_path}/schema/$component.js  &>>$log_file
+mongo_schema_setup
--- common.sh
+++ common.sh
@@ -36,3 +36,14 @@ nodejs() {
   systemctl enable $component  &>>$log_file
   systemctl restart $component  &>>$log_file
 }
+
+mongo_schema_setup() {
+  echo -e "${color} Copy MongoDB Repo file ${nocolor}"
+  cp /home/centos/roboshop-shell/mongodb.repo /etc/yum.repos.d/mongodb.repo  &>>$log_file
+
+  echo -e "${color} Install MongoDB Client ${nocolor}"
+  yum install mongodb-org-shell -y  &>>$log_file
+
+  echo -e "${color} Load Schema ${nocolor}"
+  mongo --host mongodb-dev.devopsb73.store <${app_path}/schema/$component.js  &>>$log_file
+}
