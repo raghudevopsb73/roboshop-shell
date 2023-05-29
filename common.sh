2c28c3054a5d38bba89821ca70e48125c3307b9e
Raghu K <raghuk.vit@gmail.com>
2023-05-29 07:39:35 +0530

    Payment

--- common.sh
+++ common.sh
@@ -77,3 +77,16 @@ maven() {
   systemd_setup
 
 }
+
+python() {
+  echo -e "${color} Install Python ${nocolor}"
+  yum install python36 gcc python3-devel -y &>>/tmp/roboshop.log
+
+  app_presetup
+
+  echo -e "${color} Install Application Dependencies ${nocolor}"
+  cd /app
+  pip3.6 install -r requirements.txt &>>/tmp/roboshop.log
+
+  systemd_setup
+}
