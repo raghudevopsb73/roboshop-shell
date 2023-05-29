a999b1fdfafbd3959d7b74620122cb5a2c1d9057
Raghu K <raghuk.vit@gmail.com>
2023-05-29 08:58:49 +0530

    Payment

--- common.sh
+++ common.sh
@@ -37,7 +37,7 @@ app_presetup() {
 systemd_setup() {
   echo -e "${color} Setup SystemD Service  ${nocolor}"
   cp /home/centos/roboshop-shell/$component.service /etc/systemd/system/$component.service  &>>$log_file
-  sed -i -e "s/roboshop_app_password/$roboshop_app_password/" /home/centos/roboshop-shell/$component.service
+  sed -i -e "s/roboshop_app_password/$roboshop_app_password/"  /etc/systemd/system/$component.service
 
   if [ $? -eq 0 ]; then
     echo SUCCESS
